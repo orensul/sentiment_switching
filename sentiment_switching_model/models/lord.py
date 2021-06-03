15eeaf2c65d16a820c705e82e656402f6a196097
Oren Sultan <orensultan@Orens-MBP.018.co.il>
2021-06-03 11:03:45 +0300

    add lord

---
+++ sentiment_switching_model/models/lord.py
@@ -0,0 +1,9 @@
+class Lord:
+    def __init__(self):
+        pass
+
+    def train(self, text_sequence_lengths, one_hot_labels, num_labels, data_size,
+              validation_actual_word_lists, validation_sequences, validation_sequence_lengths, validation_labels):
+        print(1)
+        print(1)
+
